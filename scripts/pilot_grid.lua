1560fb926002b84433a9f2c1656cbf6590716ced
NamesAreHard6600 <109428297+NamesAreHard6600@users.noreply.github.com>
2023-01-22 17:32:20 -0800

    Int Changes
    A lot. Optimized all the code, add cases for two click weapons, showed a preview, etc.

--- scripts/pilot_grid.lua
+++ scripts/pilot_grid.lua
@@ -28,11 +28,8 @@ function this:init(mod)
 	require(mod.scriptPath .."libs/pilotSkill_tooltip").Add(pilot.Skill, PilotSkill("Wireless Shielding", "Grants all buildings a temporary shield before attacking, and removes them after."))
 
 	-- art, icons, animations
-
-
-	--Skill
-	--ShockSkill = {}
-	--It's all in the hooks
+	modApi:appendAsset("img/combat/icons/NAH_temp_shield.png",mod.resourcePath.."img/combat/icons/NAH_temp_shield.png")
+		Location["combat/icons/NAH_temp_shield.png"] = Point(-13,5)
 end
 
 --LOG(tostring(Board:GetPawn(0)))   TESTING YOU CAN DELETE THIS
@@ -48,6 +45,72 @@ local function saveState(mission, point)
 	end
 end
 
+local function NAH_GridShield(mission, skillEffect)
+	--Save Shielded Buildings in Weapon
+	mission.buildingStates = {}
+	for i = 1, skillEffect.effect:size() do
+		local damage = skillEffect.effect:index(i);
+		if damage.iShield == EFFECT_CREATE then
+			local point = damage.loc
+			if Board:IsBuilding(point) then
+				mission.buildingStates[point:GetString()] = true
+			end
+		end
+	end
+	local buildings = extract_table(Board:GetBuildings())
+	--Shield needs to go in front, which requires making a copy, reseting, and  then adding the copy back in
+	--Save old effect
+	local oldEffect = skillEffect.effect
+	local oldEffectCopy = DamageList()
+	--Make a copy
+	for i = 1, oldEffect:size() do
+		local oldDamage = oldEffect:index(i);
+		oldEffectCopy:push_back(oldDamage)
+	end
+	--Reset
+	skillEffect.effect = DamageList()
+	--Add Buildings
+	for _, point in pairs(buildings) do
+		if Board:GetTerrain(point) ~= TERRAIN_RUBBLE then --Dead buildings
+			saveState(mission, point)
+			local damage = SpaceDamage(point)
+			damage.iShield = EFFECT_CREATE
+			skillEffect.effect:push_back(damage)
+		end
+	end
+	--Add in old effects
+	for i = 1, oldEffectCopy:size() do
+		local oldDamage = oldEffectCopy:index(i);
+		skillEffect.effect:push_back(oldDamage)
+	end
+	--Unfortunalty doesn't override damage and stuff, but still is custom
+	--Could maybe add bHide to all attacks on buildings, but that could cause
+	--issues for push and could just not work
+	for _, point in pairs(buildings) do
+		local image = SpaceDamage(point)
+		image.sImageMark = "combat/icons/NAH_temp_shield.png"
+		skillEffect.effect:push_back(image)
+	end
+end
+
+local function NAH_GridUnshield(mission)
+	local effect = SkillEffect()
+	effect:AddDelay(0.4)
+	--skillEffect:AddDelay(1)
+	local buildings = extract_table(Board:GetBuildings())
+	for _, point in pairs(buildings) do
+		if Board:IsBuilding(point) then
+			local damage = SpaceDamage(point, 0)
+			if mission.buildingStates[point:GetString()] then
+				damage.iShield = EFFECT_CREATE
+			else
+				damage.iShield = -1
+			end
+			effect:AddDamage(damage)
+		end
+	end
+	Board:AddEffect(effect) --This is weird implementaion but the better implementaion I was using earlier wasn't working so...
+end
 
 function this:load(modApiExt, options)
 	--[[
@@ -56,60 +119,29 @@ function this:load(modApiExt, options)
 		{ main = "Pilot_Skill_AZ" },
 	})
 	]]--
-	modApiExt:addSkillStartHook(function(mission, pawn, weaponId, p1, p2)
-		if pawn:IsAbility(pilot.Skill) and tostring(weaponId) ~= "Move" then
-			local board_size = Board:GetSize()
-			for i = 0,  board_size.x - 1 do
-				for j = 0, board_size.y - 1 do
-					local curr = Point(i,j)
-					if Board:IsBuilding(curr) then
-						saveState(mission, curr)
-						Board:SetShield(curr, true)
-					end
-				end
-			end
+	--for k, v in pairs(GetCurrentMission().buildingStates) do LOG(k); LOG(v) end
+	modApiExt:addSkillBuildHook(function(mission, pawn, weaponId, p1, p2, skillEffect)
+		if pawn and pawn:IsAbility(pilot.Skill) and tostring(weaponId) ~= "Move" then
+			NAH_GridShield(mission,skillEffect)
 		end
 	end)
 
-	modApiExt:addSkillBuildHook(function(mission, pawn, weaponId, p1, p2, skillEffect)
+	modApiExt:addFinalEffectBuildHook(function(mission, pawn, weaponId, p1, p2, p3, skillEffect)
 		if pawn and pawn:IsAbility(pilot.Skill) and tostring(weaponId) ~= "Move" then
-			mission.buildingStates = {}
-			for i = 1, skillEffect.effect:size() do
-				local damage = skillEffect.effect:index(i);
-				if damage.iShield == 1 then
-					local point = damage.loc
-					if Board:IsBuilding(point) then
-						mission.buildingStates[point:GetString()] = true
-					end
-				end
-			end
+			NAH_GridShield(mission,skillEffect)
 		end
 	end)
 
 	modApiExt:addSkillEndHook(function(mission, pawn, weaponId, p1, p2)--, skillEffect)
 		if pawn:IsAbility(pilot.Skill) and tostring(weaponId) ~= "Move" then
-			local effect = SkillEffect()
-			effect:AddDelay(0.4)
-			--skillEffect:AddDelay(1)
-			local board_size = Board:GetSize()
-			for i = 0,  board_size.x - 1 do
-				for j = 0, board_size.y - 1 do
-					local curr = Point(i,j)
-					if Board:IsBuilding(curr) then
-						-- Previewer Attempts
-						local damage = SpaceDamage(curr, 0)
-						if mission.buildingStates[curr:GetString()] then
-							damage.iShield = 1
-						else
-							damage.iShield = -1
-						end
-						effect:AddDamage(damage)
-					end
-				end
-			end
-			Board:AddEffect(effect) --This is weird implementaion but the better implementaion I was using earlier wasn't working so...
+			NAH_GridUnshield(mission)
 		end
+	end)
 
+	modApiExt:addFinalEffectEndHook(function(mission, pawn, weaponId, p1, p2, p3)
+		if pawn:IsAbility(pilot.Skill) and tostring(weaponId) ~= "Move" then
+			NAH_GridUnshield(mission)
+		end
 	end)
 end
 
